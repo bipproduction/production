12949e193c28b243132b2f383e48a6e643fc6f11
bipproduction <bip.production.js@gmail.com>
2022-12-21 12:50:23 +0800

    ya

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const password = document.getElementById("password");
 
 masuk.onclick = () => {
     if (user.value == "bip" && password.value == "Production_123") {
-        document.location.href = "index.md";
+        document.location.href = "laporan.html";
     } else {
         alert("dalam hitunagan ke tiga akan meledak")
     }
--- laporan.js
+++ laporan.js
@@ -1,6 +1,13 @@
 const app = document.getElementById("app");
-const datanya = `
-
-
-`
-app.innerHTML = marked.parse('# Marked in the browser\n\nRendered by **marked**.');
+var rawFile = new XMLHttpRequest();
+rawFile.open("GET", "index.md", false);
+rawFile.onreadystatechange = function () {
+    if (rawFile.readyState === 4) {
+        if (rawFile.status === 200 || rawFile.status == 0) {
+            var allText = rawFile.responseText;
+            // alert(allText);
+            app.innerHTML = marked.parse(allText);
+        }
+    }
+}
+rawFile.send(null);
